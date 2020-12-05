62bedafba5f6ab0bc1bec9bba27b251b6b0bf817
Julien Bloit <julien.bloit@gmail.com>
2020-12-05 22:59:28 +0100

    introduce Ring class. Note on controls specific ring On/off in white

--- main.py
+++ main.py
@@ -15,6 +15,32 @@ NUM_PIXELS = NUM_STEPS * NUM_RINGS
 strip = neopixel.NeoPixel (DATA_PIN, NUM_PIXELS, pixel_order=ORDER, brightness=BRIGHTNESS)
 midiInput = mido.get_input_names()[0]
 
+## RINGS
+class Ring:
+	def __init__(self, index, note, control):
+		self.index = index
+		self.note = note
+		self.control = control
+		self.start = index * NUM_STEPS
+		self.end = index * NUM_STEPS + NUM_STEPS - 1
+		self.alpha = 1
+		print('RING {} CTOR : range({}-{})'.format(index, self.start, self.end))
+
+	def noteOn(self, pitch):
+		print('ON.PITCH IN {}, internal note {}'.format(pitch, self.note))
+		if (pitch==self.note):
+			for i in range (self.start, self.end+1):
+				strip[i]= (int(alpha*255),int(alpha*255),int(alpha*255))
+	def noteOff(self, pitch):
+		print('OFF.PITCH IN {}, internal note {}'.format(pitch, self.note))
+		if (pitch==self.note):
+			for i in range (self.start, self.end+1):
+				strip[i]= (0, 0, 0)
+
+
+ring0 = Ring(0, 36, 1)
+ring1 = Ring(1, 37, 2)
+
 ## MAIN
 print('waiting for MIDI events from input : {}'.format(midiInput))
 
@@ -22,10 +48,13 @@ alpha = 1;
 port = mido.open_input(midiInput)
 for msg in port:
 	if (msg.type=='note_on'):
-		strip[0] = (int(alpha*255),int(alpha*255),int(alpha*255))
+		print(msg.note)
+		ring0.noteOn(msg.note)
+		ring1.noteOn(msg.note)
+
 	if (msg.type=='note_off'):
-		strip[0] = (0,0,0)
+                ring0.noteOff(msg.note)
+                ring1.noteOff(msg.note)
+
 	if (msg.type=='control_change'):
 		alpha = msg.value/127
-		print('alpha: {}'.format(alpha))
-
