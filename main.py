4adafeaedf1b03318ae4a16d5a038dbe26a37972
Julien Bloit <julien.bloit@gmail.com>
2020-12-06 15:08:20 +0100

    change mode on program change

--- main.py
+++ main.py
@@ -25,25 +25,42 @@ class Ring:
 		self.end = index * NUM_STEPS + NUM_STEPS - 1
 		self.alpha = 1
 		self.isOn = False
+		self.program = 0 
 		print('RING {} CTOR : range({}-{})'.format(index, self.start, self.end))
 
 	def noteOn(self, pitch):
-		#print('ON.PITCH IN {}, internal note {}'.format(pitch, self.note))
 		if (pitch==self.note):
-			self.isOn = True
-			strip[self.start:self.end+1]= [(int(self.alpha*255),int(self.alpha*255),int(self.alpha*255))] * NUM_STEPS
+			if (self.program == 0):
+				print('note on program 0')
+				self.isOn = True
+				strip[self.start:self.end+1] = [(int(self.alpha*255),int(self.alpha*255),int(self.alpha*255))] * NUM_STEPS
+
+			elif(self.program == 1):
+				self.isOn = True
+				strip[self.start:self.end+1] = [(255,0,100)] * NUM_STEPS
+
+			elif(self.program == 2):
+				self.isOn = True
+				strip[self.start:self.end+1] = [(100,30,200)] * NUM_STEPS
 
 	def noteOff(self, pitch):
-		#print('OFF.PITCH IN {}, internal note {}'.format(pitch, self.note))
 		if (pitch==self.note):
 			self.isOn = False
-			strip[self.start:self.end+1]= [(0, 0, 0)] * NUM_STEPS
+			strip[self.start:self.end+1] = [(0, 0, 0)] * NUM_STEPS
 
 	def cc(self, ccNum, ccVal):
 		if (ccNum==self.control):
 			self.alpha = ccVal/127
 			if (self.isOn):
 				self.noteOn(self.note)
+
+	def pc(self, progVal):
+		if (progVal>2):
+			progVal = 2
+		if (progVal<0):
+			progVal = 0
+		self.program = progVal
+
 
 ring0 = Ring(0, 36, 1)
 ring1 = Ring(1, 37, 2)
@@ -64,3 +81,8 @@ for msg in port:
 	if (msg.type=='control_change'):
 		ring0.cc(msg.control, msg.value)
 		ring1.cc(msg.control, msg.value)
+
+	if (msg.type=='program_change'):
+		ring0.pc(msg.program)
+		ring1.pc(msg.program)
+
