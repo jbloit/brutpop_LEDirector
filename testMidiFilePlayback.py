f40caef8214bcf7ad8fee18107cd8bf69b11e6d8
Your Name <you@example.com>
2021-03-26 14:48:12 +0000

    a looping midifile, with a playback speed that can change

--- testMidiFilePlayback.py
+++ testMidiFilePlayback.py
@@ -1,7 +1,14 @@
+import time
 from mido import MidiFile
 
-mid = MidiFile('16notesAscending.mid')
+mid = MidiFile('16notesAscending.mid');
 
-for msg in mid.play():
-    print(msg)
+slowDownFactor = 1;
 
+print ("this file is this long: ");
+print (mid.length);
+
+while True:
+	for msg in mid:
+    		time.sleep(msg.time * slowDownFactor)
+    		print(msg)
