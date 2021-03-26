1b39eaedd113d45d9687f7da2ec160b04e893138
Julien Bloit <julien.bloit@gmail.com>
2021-03-26 15:50:56 +0100

    code format

--- testMidiFilePlayback.py
+++ testMidiFilePlayback.py
@@ -1,14 +1,14 @@
 import time
 from mido import MidiFile
 
-mid = MidiFile('16notesAscending.mid');
+mid = MidiFile('16notesAscending.mid')
 
-slowDownFactor = 1;
+slowDownFactor = 1
 
-print ("this file is this long: ");
-print (mid.length);
+print("this file is this long: ")
+print(mid.length)
 
 while True:
-	for msg in mid:
-    		time.sleep(msg.time * slowDownFactor)
-    		print(msg)
+    for msg in mid:
+        time.sleep(msg.time * slowDownFactor)
+        print(msg)
