cfed27fa64a74f95cad6ff603c24468d40474a6d
Your Name <you@example.com>
2021-11-16 10:34:12 +0000

    first 2 modes work with 8 rings

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import mido
 ## CONSTANTS
 DATA_PIN = board.D18
 NUM_STEPS = 16
-NUM_RINGS = 2
+NUM_RINGS = 8
 ORDER = neopixel.RGB
 BRIGHTNESS = 0.7
 NUM_PIXELS = NUM_STEPS * NUM_RINGS
@@ -100,30 +100,59 @@ class Ring:
 			progVal = 0
 		self.mode = progVal
 
+
+## Create the 8 ring objects
 ring0 = Ring(0, 36, 1)
 ring1 = Ring(1, 37, 2)
+ring2 = Ring(2, 38, 3)
+ring3 = Ring(3, 39, 4)
+ring4 = Ring(4, 40, 5)
+ring5 = Ring(5, 41, 6)
+ring6 = Ring(6, 42, 7)
+ring7 = Ring(7, 43, 8)
 
 ## MAIN
 print('waiting for MIDI events from input : {}'.format(midiInput))
 
 port = mido.open_input(midiInput)
 for msg in port:
-
-	print("$$$$$$$$$$$$$$$$ NEW MESSAGE $$$$$$$$$$$$$$$")
-	print(msg.dict())
-
 	if (msg.type=='note_on'):
 		ring0.noteOn(msg.note)
 		ring1.noteOn(msg.note)
+		ring2.noteOn(msg.note)
+		ring3.noteOn(msg.note)
+		ring4.noteOn(msg.note)
+		ring5.noteOn(msg.note)
+		ring6.noteOn(msg.note)
+		ring7.noteOn(msg.note)
 
 	if (msg.type=='note_off'):
 		ring0.noteOff(msg.note)
 		ring1.noteOff(msg.note)
+		ring2.noteOff(msg.note)
+		ring3.noteOff(msg.note)
+		ring4.noteOff(msg.note)
+		ring5.noteOff(msg.note)
+		ring6.noteOff(msg.note)
+		ring7.noteOff(msg.note)
+
 
 	if (msg.type=='control_change'):
 		ring0.cc(msg.control, msg.value)
 		ring1.cc(msg.control, msg.value)
+		ring2.cc(msg.control, msg.value)
+		ring3.cc(msg.control, msg.value)
+		ring4.cc(msg.control, msg.value)
+		ring5.cc(msg.control, msg.value)
+		ring6.cc(msg.control, msg.value)
+		ring7.cc(msg.control, msg.value)
 
 	if (msg.type=='program_change'):
 		ring0.pc(msg.program)
 		ring1.pc(msg.program)
+		ring2.pc(msg.program)
+		ring3.pc(msg.program)
+		ring4.pc(msg.program)
+		ring5.pc(msg.program)
+		ring6.pc(msg.program)
+		ring7.pc(msg.program)
